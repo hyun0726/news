143885731429053aaad0cdb40f306adce8eba887
hyun0726 <pxm0903@naver.com>
2024-08-20 04:03:00 +0900

    newsproject0820

---
+++ NEWS_0801/java/com/korea/news/News0805Application.java
@@ -0,0 +1,13 @@
+package com.korea.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class News0805Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(News0805Application.class, args);
+	}
+
+}
